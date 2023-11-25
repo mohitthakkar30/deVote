6699787847c51bf19874f6c68d03dc4327401e55
Mohit Thakkar <thakkarmohit90@gmail.com>
2023-11-25 10:27:30 +0530

    Loading Spinner Added

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ import { ethers, Signer } from "ethers";
 import { fetchSigner } from "@wagmi/core";
 import { useAccount } from "wagmi";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { CircularProgress } from '@mui/material';
+
 
 const Home: NextPage = () => {
   const [name, setName] = useState("");
@@ -14,8 +16,13 @@ const Home: NextPage = () => {
   const [response, setResponse] = useState([0, 0, 0, 0]);
   const contractAddress = "0xcFF76d06F90af2668d7a259a2C12C3b88a1A13F3";
   const votee = ["Vitalik", "JD", "Satoshi", "Sandeep"];
+  const [isLoading, setIsLoading] = useState(false);
+  const [addedVoter, setAddedVoter] = useState(false);
+  const [voted, setVoted] = useState(false);
+
 
   const getResult = async () => {
+    setIsLoading(true);
     const res: any = [];
     const signer: any = await fetchSigner();
     const contract = new ethers.Contract(contractAddress, ABI, signer);
@@ -24,6 +31,7 @@ const Home: NextPage = () => {
         await contract.getVoteTally(votee[i]).then((res: any) => res.toString())
       );
     }
+    setIsLoading(false);
     setResponse(res);
 
     // console.log("res - ", res);
@@ -31,26 +39,32 @@ const Home: NextPage = () => {
   // const { address } = useAccount();
   const vote = async () => {
     try {
+      setVoted(true);
       const signer: any = await fetchSigner();
       const contract = new ethers.Contract(contractAddress, ABI, signer);
       const tx = await contract.vote(name);
       await tx.wait();
       setName("");
+      setVoted(false);
       getResult();
     } catch (e: any) {
+      setVoted(false);
       alert(e.reason);
     }
   };
 
   const addVoter = async () => {
-    const signer: any = await fetchSigner();
+    setAddedVoter(true);
     try {
+      const signer: any = await fetchSigner();
       const contract = new ethers.Contract(contractAddress, ABI, signer);
       const tx = await contract.registerVoter(voterAddress);
       await tx.wait();
+      setAddedVoter(false);
       setVoterAddress("");
     } catch (e: any) {
       alert(e.reason);
+      setAddedVoter(false);
     }
   };
 
@@ -76,8 +90,8 @@ const Home: NextPage = () => {
               <option value="Satoshi">Satoshi</option>
               <option value="Sandeep">Sandeep</option>
             </select>
-            <button className="vote-btn" onClick={vote}>
-              Vote
+            <button className="vote-btn text-black" onClick={vote}>
+              {voted ? <CircularProgress /> : "Vote"}
             </button>
           </div>
           <div className="wrapper">
@@ -87,8 +101,8 @@ const Home: NextPage = () => {
               placeholder="Enter address to give permission for vote"
               onChange={(e) => setVoterAddress(e.target.value)}
             />
-            <button className="vote-btn" onClick={addVoter}>
-              Add Voter
+            <button className="vote-btn text-black" onClick={addVoter}>
+              {addedVoter ? <CircularProgress /> : "Add Voter"}
             </button>
           </div>
         </div>
@@ -96,8 +110,8 @@ const Home: NextPage = () => {
         <div className="center">
           <div className="center pd-1">
             <h2>Results</h2>
-            <button className="vote-btn result-btn" onClick={getResult}>
-              Get Result
+            <button  className="vote-btn result-btn text-black" onClick={getResult}>
+            {isLoading ? <CircularProgress /> : "Get Result"}
             </button>
           </div>
 
